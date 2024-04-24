29f84e024decf3ac567fd44a4f1ea9bc07b35437
Tomáš Kaválek <tomas.kavalek88@icloud.com>
2024-04-24 09:15:19 +0200

    Add function recursive_nth_fibo

--- fibo.py
+++ fibo.py
@@ -1,6 +1,12 @@
+def recursive_nth_fibo(n):
+    if n == 1 or n == 0:
+        return n
+    else:
+        return recursive_nth_fibo(n-1) + recursive_nth_fibo(n-2)
+
 
 def main():
-    pass
+    print(recursive_nth_fibo(100))
 
 
 if __name__ == "__main__":
